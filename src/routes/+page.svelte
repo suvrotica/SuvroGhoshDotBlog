e979669ca71fc2f0701b97d0c08c78e2e912d3c3
suvrotica <suvrotica@gmail.com>
2024-07-28 19:15:56 +0530

    chore: Add mdsvex package and configuration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
+# hello
 <h1 class="text-3xl font-bold ">
     Suvro Ghosh Dot Blog
   </h1>
-  
   <style lang="postcss">
     :global(html) {
       background-color: theme(colors.gray.100);
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,25 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$lib: 'src/lib',
+			$blog: 'src/blog',
+			$blogPosts: 'src/blog/posts',
+			$blogImages: 'src/blog/images'		}
+	  },
+  extensions: ['.svelte', '.md', '.svx'],
+  preprocess: [
+    vitePreprocess(),
+    mdsvex({
+      extensions: ['.svelte','.md', '.svx'],
+    }),
+  ],
+  
 };
+
 export default config;
